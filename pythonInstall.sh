ce33c9253f24778ef727a9cd67752ad70fbc215c
Julian Romero <jnromero@gmail.com>
2019-10-07 12:37:23 -0700

    serve files

--- pythonInstall.sh
+++ pythonInstall.sh
@@ -3,6 +3,10 @@ apt-get update -y
 sudo apt install build-essential -y
 apt install python3-dev -y
 apt install python3-pip -y
+
+#server to show files
+python3 -m SimpleHTTPServer 8765
+
 #install steep python package requirements
 pip3 install setuptools
 pip3 install wheel
