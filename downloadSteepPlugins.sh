fec71e03df94f9a147e3b300717fc6f203f5ce50
Julian Romero <jnromero@gmail.com>
2019-10-07 11:32:25 -0700

    fix link

--- downloadSteepPlugins.sh
+++ downloadSteepPlugins.sh
@@ -1,2 +1,2 @@
 #download steepPlugins to the folder /root/steep/
-https://$username:$password@github.com/jnromero/steepPlugins.git /root/steepPlugins/
+git clone https://$username:$password@github.com/jnromero/steepPlugins.git /root/steepPlugins/
