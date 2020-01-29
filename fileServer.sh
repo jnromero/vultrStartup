a323e50f3f9a067fb3b1bb0f253a7e57582dc502
Julian Romero <jnromero@gmail.com>
2020-01-29 11:34:50 -0700

    fix nohup

--- fileServer.sh
+++ fileServer.sh
@@ -1,6 +1,6 @@
 echo "Starting File Server\n" >> /myLog.txt
 cd /
-nohup python3 -m http.server 8765 > /httpserver.txt 2>&1
+nohup python3 -m http.server 8765 > /httpserver.txt 2>&1 &
 echo "File Server Started\n" >> /myLog.txt
 
 
--- restartExperiment.sh
+++ restartExperiment.sh
@@ -1,5 +1,5 @@
 echo "Restarting experiment\n" >> /myLog.txt
 #get current ip address
 myip="$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
-nohup python3 /root/experiment/files/experiment.py -l local -i $myip > /experiment.txt 2>&1
+nohup python3 /root/experiment/files/experiment.py -l local -i $myip > /experiment.txt 2>&1 &
 echo "experiment restarted\n" >> /myLog.txt
