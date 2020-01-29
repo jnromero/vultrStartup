65ce21627f6a133fccd97fdd92bf418cbe8e15dd
Julian Romero <jnromero@gmail.com>
2020-01-29 10:54:56 -0700

    add nohup

--- fileServer.sh
+++ fileServer.sh
@@ -1,10 +1,5 @@
 echo "Starting File Server\n" >> /myLog.txt
-#start screen to run the program in 
-screen -dmS fileServerScreen
-
-#start server on the screen called serverScreen
-screen -S fileServerScreen -p 0 -X stuff 'cd /\n'
-screen -S fileServerScreen -p 0 -X stuff 'python3 -m http.server 8765\n'
+nohup python3 -m http.server 8765
 echo "File Server Started\n" >> /myLog.txt
 
 
