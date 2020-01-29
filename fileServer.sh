936f8634df6ba6b4b7ebfbdf85e3068bee643d79
Julian Romero <jnromero@gmail.com>
2020-01-29 16:30:17 -0700

    move html file

--- fileServer.sh
+++ fileServer.sh
@@ -3,5 +3,8 @@ cd /
 nohup python3 -m http.server 8765 > /httpserver.txt 2>&1 &
 echo "File Server Started\n"
 
+cp /root/scripts/index.html /index.html
+cp /root/scripts/checkStatusFile.py /checkStatusFile.py
+
 
 
