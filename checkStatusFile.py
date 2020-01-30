f385f6f6880ac6b5db14fad788382dc3f84cf169
Julian Romero <jnromero@gmail.com>
2020-01-29 17:09:03 -0700

    enable cgi

--- checkStatusFile.py
+++ checkStatusFile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 print("Content-type: text/plain\n") 
 
--- fileServer.sh
+++ fileServer.sh
@@ -1,6 +1,6 @@
 echo "Starting File Server\n"
 cd /
-nohup python3 -m http.server 8765 > /httpserver.txt 2>&1 &
+nohup python3 -m http.server 8765 --cgi > /httpserver.txt 2>&1 &
 echo "File Server Started\n"
 
 mkdir /cgi-bin
