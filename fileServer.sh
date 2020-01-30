d04657a2b432f24f8fb974522a352b1c45aa386a
Julian Romero <jnromero@gmail.com>
2020-01-29 17:01:36 -0700

    enable cgi

--- fileServer.sh
+++ fileServer.sh
@@ -3,5 +3,9 @@ cd /
 nohup python3 -m http.server 8765 > /httpserver.txt 2>&1 &
 echo "File Server Started\n"
 
+mkdir /cgi-bin
 cp /root/scripts/index.html /index.html
-cp /root/scripts/checkStatusFile.py /checkStatusFile.py
+cp /root/scripts/checkStatusFile.py /cgi-bin/checkStatusFile.py
+
+chmod -R 777 /cgi-bin
+chmod 777 /cgi-bin/checkStatusFile.py
