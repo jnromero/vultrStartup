9ece55859e795cf6bc673d83d2c556e2d795e380
Julian Romero <jnromero@gmail.com>
2021-02-08 13:55:22 -0700

    remove custom

--- everything.sh
+++ everything.sh
@@ -2,7 +2,7 @@
 /root/scripts/fileServer.sh
 /root/scripts/makeBashrc.sh
 /root/scripts/pythonInstall.sh
-/root/scripts/custom.sh
+# /root/scripts/custom.sh
 /root/scripts/downloadSteep.sh
 /root/scripts/downloadExperiment.sh
 /root/scripts/downloadSteepPlugins.sh
