c9f4cad55146523132b23a859819d8325c4bca48
Julian Romero <jnromero@gmail.com>
2021-02-08 13:38:01 -0700

    revert steepPlugins

--- downloadSteepPlugins.sh
+++ downloadSteepPlugins.sh
@@ -3,6 +3,8 @@ if [[ $steepPluginsVersion != "" ]]; then
 	rm -r /root/steepPlugins/
 	git clone https://$gitUsername:$gitPassword@github.com/jnromero/steepPlugins.git /root/steepPlugins/
 	if [[ $steepPluginsVersion != "latest" ]]; then
+		echo "Reverting to Experiment Version - $experimentVersion \n"
+		cd /root/steepPlugins/
 		git checkout $steepPluginsVersion
 	fi
 	echo "Steep Plugins Downloaded\n"
