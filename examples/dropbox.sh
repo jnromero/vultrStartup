58f9aa8ffbf692556fd76af85c79a4fafeed136e
Julian Romero <jnromero@gmail.com>
2021-02-08 13:10:27 -0700

    dropbox update

--- examples/dropbox.sh
+++ examples/dropbox.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-export steepVersion="none"
-export experimentVersion="none"
+git clone https://github.com/jnromero/vultrStartup.git /root/scripts/
 
+cat <<EOT >> /root/scripts/custom.sh
+echo "Custom Steep Download"
+sudo apt-get install unzip
+curl -L https://www.dropbox.com/sh/e8baz0fpee8q6q1/AAB052wZy_J349eXvyzYxTSoa?dl=1 > allFolders.zip
+unzip allFolders.zip -d /root/allFolders/
+mv  /root/allFolders/steep/ /root/steep/
+mv  /root/allFolders/BDMmmn/ /root/experiment/
+pip3 install -r /root/steep/requirements.txt
+echo "Custom Steep Downloaded\n"
+EOT
+chmod 777 /root/scripts/custom.sh
 
-git clone https://$gitUsername:$gitPassword@github.com/jnromero/vultrStartup.git /root/scripts/
 /root/scripts/everything.sh >> /myLog.txt
 
