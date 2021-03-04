c349bfef70525e49de45ffb65af56fc69d3cf7fb
Julian Romero <jnromero@gmail.com>
2021-03-04 10:24:26 -0700

    update service

--- restartExperiment.sh
+++ restartExperiment.sh
@@ -1,8 +1,10 @@
 echo "Restarting experiment\n"
-
-#kill old if needed
-kill $(ps aux | grep 'python3 /root/experiment/files/experiment.py' | awk '{print $2}')
 #get current ip address
 myip="$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
-nohup python3 /root/experiment/files/experiment.py -l local -i $myip > /experiment.txt 2>&1 &
+cp /root/scripts/experiment.service /etc/systemd/system/experiment.service
+sed -i "s/IPGOESHERE/"$myip"/" /etc/systemd/system/experiment.service 
+systemctl restart experiment
+# #kill old if needed
+# kill $(ps aux | grep 'python3 /root/experiment/files/experiment.py' | awk '{print $2}')
+# nohup python3 /root/experiment/files/experiment.py -l local -i $myip > /experiment.txt 2>&1 &
 echo "experiment restarted\n"
